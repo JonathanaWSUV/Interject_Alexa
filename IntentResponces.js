1c57be2f7cb660528a3cec8d7321709749688376
truckingmasy <jdjdjd12@live.com>
2017-08-20 22:09:09 -0700

    Working now
    Amazon Alexa IDE is buggy, not my code that was broken, needed to rewrite the intentions and utterences

---
+++ IntentResponces.js
@@ -0,0 +1,3 @@
+var SPEECH_OUTPUT = 'Hello';
+var SPEECH_OUTPUT2 = 'Hello from Interject';
+var SPEECH_OUTPUT3 = 'Hello Bryce, JD made me say this against my will, send help';
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var APP_ID = 'amzn1.ask.skill.44fcccfd-de1f-43f3-8794-d32784f5216d';
 var AlexaSkill = require('./AlexaSkill');
 var SPEECH_OUTPUT = 'Hello';
 var SPEECH_OUTPUT2 = 'Hello from Interject';
-var bryce_hello = 'Hello Bryce, JD made me say this against my will, send help';
+//var SPEECH_OUTPUT3 = 'Hello Bryce, JD made me say this against my will, send help';
 var GreeterService = function() {
  AlexaSkill.call(this, APP_ID);
 };
@@ -17,22 +17,22 @@ var helloResponseFunction = function(intent, session, response) {
 var interjectFuntion = function(intent, session, response) {
  response.tell(SPEECH_OUTPUT2);
 };
-var bryceFunction = function(intent, session, response) {
- response.tell(bryce_hello);
-};
+//var bryceFunction = function(intent, session, response) {
+// response.tell(SPEECH_OUTPUT3);
+//};
 GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
 
 
 //Intention Handelers
 
 GreeterService.prototype.intentHandlers = {
- 'HelloWorldIntent': helloResponseFunction;
+ 'HelloWorldIntent': helloResponseFunction
 };
+//GreeterService.prototype.intentHandlers = {
+// 'Test': bryceFunction
+//};
 GreeterService.prototype.intentHandlers = {
- 'Interject': interjectFuntion;
-};
-GreeterService.prototype.intentHandlers = {
- 'Bryce': bryceFunction;
+ 'Interject': interjectFuntion
 };
 
 //Pushes Responce to Lambda Handeler
