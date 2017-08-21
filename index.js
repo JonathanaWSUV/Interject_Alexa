faba2d379b760cbf7672ff942829d630a23a2c3e
truckingmasy <jdjdjd12@live.com>
2017-08-21 00:06:52 -0700

    more slight editing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var APP_ID = 'amzn1.ask.skill.44fcccfd-de1f-43f3-8794-d32784f5216d';
 var AlexaSkill = require('./AlexaSkill');
 var SPEECH_OUTPUT = 'Hello';
 var SPEECH_OUTPUT2 = 'Hello from Interject';
-//var SPEECH_OUTPUT3 = 'Hello Bryce, JD made me say this against my will, send help';
+var SPEECH_OUTPUT3 = 'Hello Bryce, JD made me say this against my will, send help';
 var GreeterService = function() {
  AlexaSkill.call(this, APP_ID);
 };
@@ -17,9 +17,9 @@ var helloResponseFunction = function(intent, session, response) {
 var interjectFuntion = function(intent, session, response) {
  response.tell(SPEECH_OUTPUT2);
 };
-//var bryceFunction = function(intent, session, response) {
-// response.tell(SPEECH_OUTPUT3);
-//};
+var bryceFunction = function(intent, session, response) {
+ response.tell(SPEECH_OUTPUT3);
+};
 GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
 
 
@@ -28,9 +28,9 @@ GreeterService.prototype.eventHandlers.onLaunch = helloResponseFunction;
 GreeterService.prototype.intentHandlers = {
  'HelloWorldIntent': helloResponseFunction
 };
-//GreeterService.prototype.intentHandlers = {
-// 'Test': bryceFunction
-//};
+GreeterService.prototype.intentHandlers = {
+ 'Test': bryceFunction
+};
 GreeterService.prototype.intentHandlers = {
  'Interject': interjectFuntion
 };
